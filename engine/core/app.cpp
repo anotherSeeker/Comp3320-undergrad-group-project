af4ae7610975874ce7e035f161822d6af9b2992a
chocolate_bar2410 <chocolatebar24.business@gmail.com>
2026-08-24 19:33:39 +1000

    fixed bugs related to drawing and file loading

--- engine/core/app.cpp
+++ engine/core/app.cpp
@@ -7,25 +7,30 @@
 #include <string>
 
 std::vector<vertex> vertices{
-    vertex{glm::vec3{-0.5, 0.0, 0}},
+    vertex{glm::vec3{-0.5, -0.5, 0}},
     vertex{glm::vec3{ 0.0, 0.5, 0}},
-    vertex{glm::vec3{ 0.5, 0.0, 0}},
+    vertex{glm::vec3{ 0.5, -0.5, 0}},
 };
 
 std::vector<GLuint> indices{
     0,1,2
 };
 
-
-const char* loadFile(std::string filePath){
+std::string loadFile(std::string filePath){
     std::ifstream file{filePath};
+    if(!file){
+        std::cerr << "cannot load file\n";
+        return "";
+    }
+    
     auto fileSize = file.seekg(0,std::ios::end).tellg();
     file.seekg(0);
 
     std::string fileContents{};
+    fileContents.resize(fileSize);
     file.read(fileContents.data(),fileSize);
 
-    return fileContents.data();
+    return fileContents;
 }
 
 bool App::init(int32_t width,int32_t height,const char* title){
@@ -33,6 +38,7 @@ bool App::init(int32_t width,int32_t height,const char* title){
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,6);
+    glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
     
     window = glfwCreateWindow(width,height,title,nullptr,nullptr);
@@ -48,24 +54,29 @@ bool App::init(int32_t width,int32_t height,const char* title){
         return false;
     };
 
+    glViewport(0,0,width,height);
+
     return true;
 }
 
 void App::run(){
     std::cout << "running\n";
 
-    Shader shader(loadFile("assets/shaders/default.vert"),loadFile("assets/shaders/default.frag"));
+    Shader shader(loadFile("../assets/shaders/default.vert").data(),loadFile("../assets/shaders/default.frag").data());
     Mesh mesh(vertices,indices);
 
     glClearColor(0.39,0.58,0.93,1.0);
 
     while(!glfwWindowShouldClose(window)){
-        mesh.draw(shader);
+        
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwPollEvents();
+        mesh.draw(shader);
+
         glfwSwapBuffers(window);
+        glfwPollEvents();
+        
     }
 }
 
