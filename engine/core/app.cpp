21c3e077ec4aff4c8b34cbb8b054c59da113a670
chocolate_bar2410 <chocolatebar24.business@gmail.com>
2026-08-24 16:45:29 +1000

    added opengl setup

--- engine/core/app.cpp
+++ engine/core/app.cpp
@@ -1,16 +1,53 @@
 #include "./app.hpp"
-#include <iostream>
+#include "renderer/mesh.hpp"
 
-bool App::init(){
+#include <iostream>
+#include <vector>
+
+std::vector<vertex> vertices{
+    glm::vec3()
+}
+
+
+bool App::init(int32_t width,int32_t height,const char* title){
     std::cout << "init\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    
+    window = glfwCreateWindow(width,height,title,nullptr,nullptr);
+
+    if(!window){
+        std::cerr << "failed to create window\n";
+        return false;
+    }
+
+    glfwMakeContextCurrent(window);
+    if(!gladLoadGL(glfwGetProcAddress)){
+        std::cerr << "failed to load openGL pointers with glad\n";
+        return false;
+    };
 
     return true;
 }
 
 void App::run(){
     std::cout << "running\n";
+
+    glClearColor(0.39,0.58,0.93,1.0);
+
+    while(!glfwWindowShouldClose(window)){
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
 }
 
 App::~App(){
     std::cout << "finished\n";
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
--- engine/core/app.hpp
+++ engine/core/app.hpp
@@ -3,8 +3,10 @@
 #include <GLFW/glfw3.h>
 
 class App{
+    private:
+    GLFWwindow* window;
     public:
-    bool init();
+    bool init(int32_t width,int32_t height,const char* title);
     void run();
 
     ~App();
