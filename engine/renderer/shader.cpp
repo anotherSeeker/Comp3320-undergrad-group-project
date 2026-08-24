2adf9548599711f4e79e9d24f1cbe1efc00c2363
chocolate_bar2410 <chocolatebar24.business@gmail.com>
2026-08-24 19:09:40 +1000

    added error detection for shaders

--- engine/renderer/shader.cpp
+++ engine/renderer/shader.cpp
@@ -1,17 +1,23 @@
 #include "shader.hpp"
 
+#include <iostream>
+
 Shader::Shader(const char* vertexSource,const char* fragmentSource){
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader,1,&vertexSource,nullptr);
     glCompileShader(vertexShader);
+    CheckError(vertexShader,"VERTEX");
 
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader,1,&fragmentSource,nullptr);
     glCompileShader(fragmentShader);
+    CheckError(fragmentShader,"FRAGMENT");
 
     id = glCreateProgram();
     glAttachShader(id,vertexShader);
     glAttachShader(id,fragmentShader);
+    glLinkProgram(id);
+    CheckError(id,"PROGRAM");
 
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
@@ -23,4 +29,24 @@ void Shader::use(){
 
 Shader::~Shader(){
     glDeleteProgram(id);
+}
+
+void Shader::CheckError(GLuint shader,const char* type){
+    GLint hasCompiled;
+    size_t logSize = 2 << 10;
+    char infoLog[logSize]; 
+
+    if(type == "PROGRAM"){
+        glGetProgramiv(shader,GL_LINK_STATUS,&hasCompiled);
+        if(hasCompiled == GL_TRUE) return;
+
+        glGetProgramInfoLog(shader,logSize,nullptr,infoLog);
+        std::cerr << "SHADER COMPILE FAILED: " << type << "\n" << infoLog << "\n";
+    } else {
+        glGetShaderiv(shader,GL_COMPILE_STATUS,&hasCompiled);
+        if(hasCompiled == GL_TRUE) return;
+
+        glGetShaderInfoLog(shader,logSize,nullptr,infoLog);
+        std::cerr << "SHADER LINKING FAILED: " << type << "\n" << infoLog << "\n";
+    }
 }
--- engine/renderer/shader.hpp
+++ engine/renderer/shader.hpp
@@ -5,6 +5,8 @@ class Shader{
     private:
     GLuint id;
 
+    void CheckError(GLuint shader,const char* type);
+
     public:
 
     Shader(const char* vertexSource,const char* fragmentSource);
