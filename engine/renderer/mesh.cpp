3a9d289e02e132cf60fea0b10649be6bfa51b592
chocolate_bar2410 <chocolatebar24.business@gmail.com>
2026-08-24 19:32:02 +1000

    fixed bugs with VAO creation and drawing

--- engine/renderer/mesh.cpp
+++ engine/renderer/mesh.cpp
@@ -2,6 +2,9 @@
 
 Mesh::Mesh(std::vector<vertex> vertices,std::vector<GLuint> indices) : vertices(vertices), indices(indices){
     glGenVertexArrays(1,&VAO);
+
+    glBindVertexArray(VAO);
+
     glGenBuffers(1,&VBO);
     glGenBuffers(1,&EBO);
 
@@ -11,20 +14,18 @@ Mesh::Mesh(std::vector<vertex> vertices,std::vector<GLuint> indices) : vertices(
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER,indices.size() * sizeof(GLuint),indices.data(),GL_STATIC_DRAW);
 
-    glVertexAttribFormat(0,3,GL_FLOAT,GL_FALSE,0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,sizeof(vertex),(void*)0);
     glEnableVertexAttribArray(0);
 }
 
 void Mesh::draw(Shader shader){
     this->bind();
     shader.use();
-    glDrawElements(GL_STATIC_DRAW,indices.size(),GL_FLOAT,indices.data());
+    glDrawElements(GL_TRIANGLES,indices.size(),GL_UNSIGNED_INT,0);
 }
 
 void Mesh::bind(){
     glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
 }
 
 void Mesh::unbind(){
