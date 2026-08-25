91435aa7d60b9a422c4290475d6e7409c451880a
Sol Scobie <scobiesolomon@gmail.com>
2026-08-26 00:27:58 +1000

    replacing file.seekg(0) with file.seekg(0, std::ios::beg) appears to fix the issue going to reattempt with later builds

--- engine/core/app.cpp
+++ engine/core/app.cpp
@@ -3,17 +3,39 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+// #include <string>
 
 std::vector<vertex> vertices{
-    vertex{glm::vec3{-0.5, 0.0, 0}},
-    vertex{glm::vec3{ 0.0, 0.5, 0}},
-    vertex{glm::vec3{ 0.5, 0.0, 0}},
+    vertex{glm::vec3{-0.5f, 0.0f, 0.0f}},
+    vertex{glm::vec3{ 0.0f, 0.5f, 0.0f}},
+    vertex{glm::vec3{ 0.5f, 0.0f, 0.0f}},
 };
 
 std::vector<GLuint> indices{
     0,1,2
 };
 
+std::string loadFile(std::string filePath){
+    std::ifstream file(filePath);
+    if(!file){
+        std::cerr << "cannot load file\n";
+        return "";
+    }
+
+    auto fileSize = file.seekg(0,std::ios::end).tellg();
+
+    //build succeeds .exe launches
+    file.seekg(0, std::ios::beg);
+    //build succeeeds .exe fails to launch with error "The procedure entry point _ZNSi5seekgESt4fposliE could not be located in the dynamic link library C:/3320-group-project\Comp3320-undergrad-group-project\sandbox\libsunked_castle.dll"
+    //file.seekg(0);
+
+    std::string fileContents{};
+    fileContents.resize(fileSize);
+    file.read(fileContents.data(), fileSize);
+
+    return fileContents;
+}
 
 bool App::init(int32_t width,int32_t height,const char* title){
     std::cout << "init\n";
