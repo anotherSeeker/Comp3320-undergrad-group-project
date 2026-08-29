31733cfa1b9d7f9506a6dda82fe213434d98cdd0
chocolate_bar2410 <chocolatebar24.business+github@gmail.com>
2026-08-29 20:33:42 +1000

    set output directory for archive files

--- engine/sunken_engine.hpp
+++ engine/sunken_engine.hpp
@@ -1,14 +1,14 @@
 #include "./core/events/event_structs.hpp"
 
 extern "C" {
-    bool skInit();
-    void skRun();
+    __declspec(dllexport) bool skInit();
+    __declspec(dllexport) void skRun();
 
-    void skEventCallback(void (*eventCallback)(int,int,void*));
-    void skListen(const char* eventName,int callback);
+    __declspec(dllexport) void skEventCallback(void (*eventCallback)(int,int,void*));
+    __declspec(dllexport) void skListen(const char* eventName,int callback);
 
-    void skLog(const char* message);
+    __declspec(dllexport) void skLog(const char* message);
 
-    void skMoveView(float x,float y,float z);
-    void skRotateView(float x,float y,float z);
+    __declspec(dllexport) void skMoveView(float x,float y,float z);
+    __declspec(dllexport) void skRotateView(float x,float y,float z);
 }
