1926ed4693caaade726dd78efd4f347d5153c04c
chocolate_bar2410 <chocolatebar24.business+github@gmail.com>
2026-08-29 20:33:12 +1000

    moved rust configs to runtime folder

---
+++ runtime/src/main.rs
@@ -0,0 +1,28 @@
+use std::ffi;
+use std::os::raw::c_char;
+
+type EventCallbackFn = extern "C" fn(i32, i32, *mut ffi::c_void);
+
+#[link(name = "sunked_castle.dll", kind = "raw-dylib")]
+unsafe extern "C" {
+    pub fn skInit() -> bool;
+    pub fn skRun();
+
+    pub fn skEventCallback(eventCallback: EventCallbackFn);
+    pub fn skListen(eventName: *const c_char, callback: i32);
+
+    pub fn skLog(message: *const c_char);
+
+    pub fn skMoveView(x: f32, y: f32, z: f32);
+    pub fn skRotateView(x: f32, y: f32, z: f32);
+}
+
+fn main() {
+    unsafe {
+        if !skInit() {
+            return;
+        };
+
+        skRun();
+    }
+}
