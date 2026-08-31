56315563b025fe5c5b7c3e03d7c798cf226ad2fc
chocolate_bar2410 <chocolatebar24.business+github@gmail.com>
2026-08-31 16:11:35 +1000

    added the ability to bind luau function to keypress

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -1,19 +1,41 @@
-use mlua::Lua;
+use mlua::{IntoLua, Lua};
 use std::ffi;
 use std::io::Read;
 
 use std::fs;
+use std::sync::{LazyLock, Mutex};
+
+use std::collections::HashMap;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+static LUAU_VM: LazyLock<Lua> = LazyLock::new(|| Lua::new());
+
+static CALLBACKS: LazyLock<Mutex<HashMap<i32, mlua::RegistryKey>>> = LazyLock::new(|| {
+    let callback_hashmap = HashMap::new();
+    Mutex::new(callback_hashmap)
+});
+
 extern "C" fn event_callback(callback: i32, event_id: i32, data: *mut ffi::c_void) {
-    if event_id as u32 == SK_EVENT_SK_EVENT_KEY_PRESS {
+    let map = CALLBACKS.lock().unwrap();
+
+    if event_id as u32 == SK_EVENT_KEY_PRESS {
         let key = unsafe {
-            let key_ptr = data as *const u8 as *const char;
+            let key_ptr = data as *const u8;
             *key_ptr
         };
 
-        println!("{} {} {}", callback, event_id, key)
+        if map.contains_key(&callback) {
+            let reg_key = map.get(&callback).unwrap();
+            let event_fn = LUAU_VM
+                .registry_value::<mlua::Function>(reg_key)
+                .expect(format!("Could not find callback for event {}", callback).as_str());
+
+            let mut args = mlua::MultiValue::new();
+            args.push_back(key.into_lua(&LUAU_VM).unwrap());
+
+            event_fn.call::<()>(args).expect("Event errored");
+        }
     }
 }
 
@@ -25,20 +47,22 @@ fn read_file(path: &str) -> String {
     contents
 }
 
-fn setup_luau(luau_vm: &Lua) -> Result<(), Box<dyn std::error::Error>> {
-    let chunk = luau_vm.load(read_file("./demo_scripts/demo.luau"));
+fn setup_luau() -> Result<(), Box<dyn std::error::Error>> {
+    let callback_count: i32 = 0;
 
-    let globals = luau_vm.globals();
-    let camera_lib = luau_vm.create_table()?;
+    let chunk = LUAU_VM.load(read_file("./demo_scripts/demo.luau"));
 
-    let camera_move_fn = luau_vm.create_function(|_, (x, y, z): (f32, f32, f32)| {
+    let globals = LUAU_VM.globals();
+    let camera_lib = LUAU_VM.create_table()?;
+
+    let camera_move_fn = LUAU_VM.create_function(|_, (x, y, z): (f32, f32, f32)| {
         unsafe {
             skMoveView(x, y, z);
         }
         Ok(())
     })?;
 
-    let camera_rot_fn = luau_vm.create_function(|_, (x, y, z): (f32, f32, f32)| {
+    let camera_rot_fn = LUAU_VM.create_function(|_, (x, y, z): (f32, f32, f32)| {
         unsafe {
             skRotateView(x, y, z);
         }
@@ -50,10 +74,17 @@ fn setup_luau(luau_vm: &Lua) -> Result<(), Box<dyn std::error::Error>> {
 
     globals.set("camera", camera_lib)?;
 
-    let input_lib = luau_vm.create_table()?;
+    let input_lib = LUAU_VM.create_table()?;
 
-    let connect_key_press = luau_vm.create_function(|_, arg_fn: mlua::Function| {
-        arg_fn.call::<()>(())?;
+    let connect_key_press = LUAU_VM.create_function(move |_, arg_fn: mlua::Function| {
+        let key = LUAU_VM.create_registry_value(arg_fn)?;
+        let mut map = CALLBACKS.lock().unwrap();
+
+        map.insert(callback_count, key);
+        unsafe {
+            let eventbinding = ffi::CString::new("KeyPress").unwrap();
+            skListen(eventbinding.as_ptr(), callback_count);
+        }
 
         Ok(())
     })?;
@@ -68,8 +99,6 @@ fn setup_luau(luau_vm: &Lua) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let luau_vm = &Lua::new();
-
     unsafe {
         if !skInit() {
             return Ok(());
@@ -79,7 +108,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         skEventCallback(Some(event_callback));
         skListen(eventbinding.into_raw(), 1);
 
-        setup_luau(luau_vm)?;
+        setup_luau()?;
 
         skRun();
     }
